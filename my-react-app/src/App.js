bf3d48245cfc36d5b31cf710cebcece3a04305e6
regitaasri <72766308+regitaasri@users.noreply.github.com>
2023-05-24 20:41:27 +0700

    Update

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,8 +1,4 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import SearchBar from "./components/SearchBar";
 
